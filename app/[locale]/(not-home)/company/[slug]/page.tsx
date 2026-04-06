11cb51d180c8706297bd87ff7337e733c4671cd8
ligogroupid <ligo.groupid@gmail.com>
2026-04-06 15:57:45 +0700

    remove sticky border

--- app/[locale]/(not-home)/company/[slug]/page.tsx
+++ app/[locale]/(not-home)/company/[slug]/page.tsx
@@ -196,10 +196,10 @@ export default async function Page__CompanyDetails({ params }: Props) {
         {/* [iOS-safari-glitch] translate3d forces GPU compositing layer so Safari respects z-index on older iOS*/}
         {/* IMAGE COMPANY STRENGTH — inside wrapper so it stays on colored bg, scrolls naturally */}
         {company.strengths_image && (
-          <div className="absolute left-0 top-0  border border-dashed border-green-300 -translate-y-full pointer-events-none">
+          <div className="absolute left-0 top-0  -translate-y-full pointer-events-none">
             <div className="pointer-events-none relative z-1 max-w-lg">
               <img
-                className="w-full object-contain border-dotted border-4 border-fuchsia-600 pointer-events-none"
+                className="w-full object-contain pointer-events-none"
                 alt={`${company.name[loc]} strength`}
                 src={company.strengths_image}
               />
